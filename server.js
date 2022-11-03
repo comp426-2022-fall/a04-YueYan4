74710b67146eac1f5e3d2c19bf51b8dcaff20831
YueYan4 <yueyan@email.unc.edu>
2022-11-03 14:26:51 -0400

    test

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const express = require('express')
 
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+//app.use(express.json())
+//app.use(express.urlencoded({extended:true}))
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 var port = 5000
 
 //import minimist from 'minimist'
@@ -29,36 +34,50 @@ var rolls = 1
 var dice = 2
 
 app.get('/app/roll/', (req,res,next) => {
-  if(Object.keys(req.body).length != 0) {
-    sides = req.body.sides
-    rolls = req.body.rolls
-    dice = req.body.dice
+  console.log(req.body)
+  var isSides = req.query.sides;
+  if(isSides){
+   sides = isSides
   }
-  res.send(require('./lib/lib/roll.js').roll(sides, dice, rolls))
+  var isRolls = req.query.rolls
+  if(isRolls){
+   rolls = isRolls
+  }
+  var isDice = req.query.dice
+  if(isDice){
+   dice = isDice
+  }
+  res.send(require('./lib/lib/roll.js').roll(sides, dice, rolls)).end()
 })
 
 app.get('/app/roll/:sides', (req,res,next) => {
   sides = req.params.sides
-  res.send(require('./lib/lib/roll.js').roll(sides, dice, rolls))
+  res.send(require('./lib/lib/roll.js').roll(sides, dice, rolls)).end()
 })
 
 app.get('/app/roll/:sides/:dice/', (req,res,next) => {
   sides = req.params.sides
   dice = req.params.dice
-  res.send(require('./lib/lib/roll.js').roll(sides, dice, rolls))
+  res.send(require('./lib/lib/roll.js').roll(sides, dice, rolls)).end()
 })
 
 app.get('/app/roll/:sides/:dice/:rolls', (req,res,next) => {
   sides = req.params.sides
   dice = req.params.dice
   rolls = req.params.rolls
-  res.send(require('./lib/lib/roll.js').roll(sides, dice, rolls))
+  res.send(require('./lib/lib/roll.js').roll(sides, dice, rolls)).end()
 })
 
 app.use((req, res) => {
+//  console.log(req.body)
+  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+//  console.log(fullUrl)
   res.status(404).send("404 NOT FOUND").end()
 })
 
+app.get('/', () => {
+  console.log("Server listening on port " + port)
+})
 app.listen(port, (req, res)=> {
  // console.log("Server listening on port " + port)
 })
